5f782fd2e73f725e6de88f71301a5b838d723067
Rakib Ahmed <rakibofficial@gmail.com>
2020-05-28 05:02:21 +0600

    Update 2. Max-Loot.cpp

--- Week 3/2. Max-Loot.cpp
+++ Week 3/2. Max-Loot.cpp
@@ -3,51 +3,51 @@ using namespace std;
 
 int get_max(int n, vector<int> value, vector<int> weight)
 {
-	double maxx = 0;
-	int max_index = 0;
+    double maxx = 0;
+    int max_index = 0;
 	
-	for (size_t i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
+    {
+        if (weight[i] != 0 && (double)value[i] / weight[i] > maxx)
 	{
-		if (weight[i] != 0 && (double)value[i] / weight[i] > maxx)
-		{
-			maxx = (double)value[i] / weight[i];
-			max_index = i;
-		}
+	    maxx = (double)value[i] / weight[i];
+	    max_index = i;
 	}
-	return max_index;
+    }
+    return max_index;
 }
 
 double knapsack(vector<int> value, vector<int> weight, int n, int w)
 {
-	double val = 0.0;
+    double val = 0.0;
 	
-	for (int i = 0; i < n; i++)
-	{
-		if (w == 0)
-			return val;
+    for (int i = 0; i < n; i++)
+    {
+        if (w == 0)
+	    return val;
 		
-		int index = get_max(n, value, weight);
-		int m = min(w, weight[index]);
-		val += m * (double)value[index] / weight[index];
-		weight[index] -= m;
-		w -= m;
-	}
-	return val;
+	int index = get_max(n, value, weight);
+	int m = min(w, weight[index]);
+	val += m * (double)value[index] / weight[index];
+	weight[index] -= m;
+	w -= m;
+    }
+    return val;
 }
 
 int main()
 {
-	int n, w;
-	cin >> n >> w;
+    int n, w;
+    cin >> n >> w;
 	
-	vector<int> value(n), weight(n);
-	for (size_t i = 0; i < n; i++)
-	{
-		cin >> value[i] >> weight[i];
-	}
+    vector<int> value(n), weight(n);
+    for (size_t i = 0; i < n; i++)
+    {
+	cin >> value[i] >> weight[i];
+    }
 	
-	double res = knapsack(value, weight, n, w);
-	cout << fixed << setprecision(4) << res << endl;
+    double res = knapsack(value, weight, n, w);
+    cout << fixed << setprecision(4) << res << endl;
 	
-	return 0;
+    return 0;
 }
